bb8771e30485bfc0d81a39d5ec9ba2d07206f0f8
420947475@qq.com <420947475@qq.com>
2018-04-02 19:19:14 +0800

    增加 辅助依赖
    增加工具类

---
+++ core/src/main/java/com/lak/core/LakCore.java
@@ -0,0 +1,8 @@
+package com.lak.core;
+
+/**
+ * Created by lawrence on 2018/4/2.
+ */
+
+public class LakCore {
+}
---
+++ tools/src/main/java/com/lak/tools/LakTools.java
@@ -0,0 +1,8 @@
+package com.lak.tools;
+
+/**
+ * Created by lawrence on 2018/4/2.
+ */
+
+public class LakTools {
+}
